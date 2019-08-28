9c60a3cbc30c6e70d65fc6440412e7b68501710c
ansaryergesh <yergeshovansar@gmail.com>
2019-08-29 00:50:48 +0600

    player_spec done

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Player do
   let(:player) { Player.new("Ansar") }
   describe "#Player" do
     it "returns name" do
-      expect(player.name).to eql(name)
+      expect(player.name).to eql('Ansar')
     end
-    it "Returns player's object score attribute initialsed to 0" do
+    it "Player's score amount is 0 at the begining" do
       expect(player.amount).to eql(0)
     end
   end
 
   describe '#Player.choice' do
-    it 'Returns a player object with the attribute choice set to the given value' do
+    it 'Returns a players piece' do
       player.piece = 'x'
       expect(player.piece).to eql('x')
     end
-    it 'Returns a player object with the attribute choice set to the given value' do
+    it 'Returns a players piece' do
       player.piece = 'o'
       expect(player.piece).to eql('o')
     end
